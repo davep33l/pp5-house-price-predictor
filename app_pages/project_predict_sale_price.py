7a84f79d251c2f8aad889f45da39146ab916af6d
David Peel <davidpeel33@hotmail.com>
2024-09-22 13:03:55 +0000

    add function to show features to turn into widgets

--- app_pages/project_predict_sale_price.py
+++ app_pages/project_predict_sale_price.py
@@ -5,7 +5,7 @@ from src.data_management import load_pkl_file
 
 def project_predict_sale_price_body():
 
-    # load predict tenure files
+    # load predict sale price files
     version = 'v1'
     sale_price_pipe = load_pkl_file(
         f"outputs/ml_pipeline/predict_sale_price/{version}/clf_pipeline.pkl")
@@ -14,8 +14,26 @@ def project_predict_sale_price_body():
                        .to_list()
                        )
 
-    st.write("### Predict Sale Price")
+    st.write("### Predict Sale Price Interface")
 
-    st.write(sale_price_pipe)
+    st.info(f"The client is interested in predicting the house sale price from her four inherited houses and any other house in Ames, Iowa.")
+
     st.write("---")
-    st.write(sale_price_features)
+
+    # Generate Live Data
+    check_variables_for_UI(sale_price_features)
+
+
+
+def check_variables_for_UI(sale_price_features):
+    import itertools
+
+    # The widgets inputs are the features used in all pipelines (sale price)
+    # We combine them only with unique values
+    combined_features = set(
+        list(
+            itertools.chain(sale_price_features)
+        )
+    )
+    st.write(
+        f"* There are {len(combined_features)} features for the UI: \n\n {combined_features}")
